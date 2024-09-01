672eaee100915ea3773e0ff4cb6b1270440fd7f9
Shannon Robinson <srobin4922@gmail.com>
2024-09-01 11:47:38 -0400

    Cleanup default app, set up timer app structure

--- timer/src/App.js
+++ timer/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Timer App</header>
+      <div className="App-body">This is where the timer goes</div>
+      <footer className="App-footer">Prayerz</footer>
     </div>
   );
 }
